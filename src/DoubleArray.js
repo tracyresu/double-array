1a95413bd59a49e12030195295093f447299143c
tracyresurreccion <tracycarmelcresurreccion@gmail.com>
2022-02-23 16:46:51 +0800

    Create input and output containers

--- src/DoubleArray.js
+++ src/DoubleArray.js
@@ -3,7 +3,14 @@ import './DoubleArray.css';
 function DoubleArray() {
   return (
       <div className="App">
-
+          <div className="container">
+              <div className="container-label">Input</div>
+              <div className="data-label">Array</div>
+          </div>
+          <div className="container">
+              <div className="container-label">Output</div>
+              <div className="data-label">Double</div>
+          </div>
       </div>
   );
 }
