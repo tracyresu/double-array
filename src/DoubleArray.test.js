987e4e27f4c5ee8d103b325d392a767ff446ec8a
tracyresurreccion <tracycarmelcresurreccion@gmail.com>
2022-02-23 18:04:56 +0800

    Supplement test cases

--- src/DoubleArray.test.js
+++ src/DoubleArray.test.js
@@ -1,14 +1,16 @@
-import {fireEvent, render} from "@testing-library/react";
+import {cleanup, fireEvent, render} from "@testing-library/react";
 import DoubleArray from "./DoubleArray";
 
 let getByTestId;
 let inputElement;
 let outputElement;
+let invalidMsg;
 
 beforeEach(() => {
     const component = render(<DoubleArray />);
     getByTestId = component.getByTestId;
 
+    invalidMsg = getByTestId("invalid-msg-test");
     inputElement = getByTestId("input-test");
     outputElement = getByTestId("output-test");
 });
@@ -47,4 +49,45 @@ test("Change output works correctly given valid input text", () => {
 
     expect(inputElement.value).toBe("1,2");
     expect(outputElement.placeholder).toBe("2,4");
+});
+
+test("Change output works correctly given input text with float value", () => {
+    expect(inputElement.value).toBe("");
+    expect(outputElement.value).toBe("");
+    fireEvent.change(inputElement, {
+        target: {
+            value: "1,2.3,42,3.12",
+        },
+    });
+
+    expect(inputElement.value).toBe("1,2.3,42,3.12");
+    expect(outputElement.placeholder).toBe("2,4.6,84,6.24");
+});
+
+test("Change output works correctly given invalid text", () => {
+    expect(inputElement.value).toBe("");
+    expect(outputElement.value).toBe("");
+    fireEvent.change(inputElement, {
+        target: {
+            value: "3a,42",
+        },
+    });
+
+    expect(inputElement.value).toBe("3a,42");
+    expect(outputElement.placeholder).toBe("");
+    expect(invalidMsg.textContent).toBe("Please enter valid input!");
+});
+
+test("Display invalid message given invalid input text", () => {
+    fireEvent.change(inputElement, {
+        target: {
+            value: "3a,42",
+        },
+    });
+
+    expect(invalidMsg.textContent).toBe("Please enter valid input!");
+});
+
+afterEach(() => {
+    cleanup();
 });
