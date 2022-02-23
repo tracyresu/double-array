7c4b2ca8ea0e6da2f1f9ab999833739d30ccef32
tracyresurreccion <tracycarmelcresurreccion@gmail.com>
2022-02-23 16:52:12 +0800

    Add container input elements

--- src/DoubleArray.js
+++ src/DoubleArray.js
@@ -1,18 +1,37 @@
 import './DoubleArray.css';
+import React, { useState } from "react";
 
 function DoubleArray() {
-  return (
-      <div className="App">
-          <div className="container">
-              <div className="container-label">Input</div>
-              <div className="data-label">Array</div>
-          </div>
-          <div className="container">
-              <div className="container-label">Output</div>
-              <div className="data-label">Double</div>
-          </div>
-      </div>
-  );
+    const [input, setInput] = useState("");
+
+    const handleInput = (e) => {
+        setInput(e.target.value);
+        console.log(input)
+    };
+
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="container-label">Input</div>
+                <div className="data-label">Array</div>
+                <input
+                    className="input-element"
+                    data-testid="input-test"
+                    type="text"
+                    onChange={(e) => handleInput(e)}
+                />
+            </div>
+            <div className="container">
+                <div className="container-label">Output</div>
+                <div className="data-label">Double</div>
+                <input
+                className="input-element"
+                data-testid="output-test"
+                type="text"
+            />
+            </div>
+        </div>
+    );
 }
 
 export default DoubleArray;
