c6af94e86214e173fd6a68d6f4627a67cb4bc258
tracyresurreccion <tracycarmelcresurreccion@gmail.com>
2022-02-23 17:47:43 +0800

    Supplemented test cases on fireEvent change

--- src/DoubleArray.test.js
+++ src/DoubleArray.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import {fireEvent, render} from "@testing-library/react";
 import DoubleArray from "./DoubleArray";
 
 let getByTestId;
@@ -21,4 +21,30 @@ test("Component renders input correctly", () => {
 test("Component renders output correctly", () => {
     expect(outputElement).toBeTruthy;
     expect(outputElement.value).toBe("");
+});
+
+test("Change input and output works correctly given onChange event", () => {
+    expect(inputElement.value).toBe("");
+    expect(outputElement.value).toBe("");
+    fireEvent.change(inputElement, {
+        target: {
+            value: "text",
+        },
+    });
+
+    expect(inputElement.value).toBe("text");
+    expect(outputElement.value).toBe("");
+});
+
+test("Change output works correctly given valid input text", () => {
+    expect(inputElement.value).toBe("");
+    expect(outputElement.value).toBe("");
+    fireEvent.change(inputElement, {
+        target: {
+            value: "1,2",
+        },
+    });
+
+    expect(inputElement.value).toBe("1,2");
+    expect(outputElement.placeholder).toBe("2,4");
 });
